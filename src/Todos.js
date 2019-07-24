3f8a272277a7441940aba54c5f3baad054e0ea7e
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-07-23 23:28:01 -0500

    added component and corrected directory

---
+++ src/Todos.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Todos = ({todos}) => {
+    
+    const todolist = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className="collection-item" key={todo.id}>
+                    <span>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center"> You have no todo's left </p>
+    )
+    return (
+        <div className="todos collection">
+            {todolist}
+
+        </div>
+    )
+
+}
+
+export default Todos
