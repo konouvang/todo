8b27adb35fe78e7cd991d85949c7cacaf064791f
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-07-24 22:17:11 -0500

    added delete

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ class App extends Component {
       {id: 1, content: 'play some mario kart'}
     ]
   }
+  deleteTodo = (id) => {
+    console.log(id);
+  }
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
       <h1 className="center blue-text">Todo's</h1>
-      <Todos todos={this.state.todos} />
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     )
   }
